887bc735922e9719faacf95352117c94f1f748c7
Fábio Penha <fabiopenha95@outlook.com>
2023-01-11 00:48:12 -0300

    feat: axios withCredentials allowed

--- src/components/utils/api/api.ts
+++ src/components/utils/api/api.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: `${process.env.REACT_APP_URI}`
+    baseURL: `${process.env.REACT_APP_URI}`,
+    withCredentials: true
 });
