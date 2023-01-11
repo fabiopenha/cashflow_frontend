b2115c32083461ff37bf6c5480b20f819b6a1c60
Fábio Penha <fabiopenha95@outlook.com>
2023-01-11 18:01:16 -0300

    feat: create axios response interceptors

--- src/components/utils/api/api.ts
+++ src/components/utils/api/api.ts
@@ -5,3 +5,19 @@ export const api = axios.create({
 });
 
 api.defaults.withCredentials = true;
+
+let refresh = false;
+
+api.interceptors.response.use(response => response, async (error) => {
+    if(error.response.status === 401 && !refresh) {
+        refresh = true;
+        const response = await api.post('/v1/auth/refreshtoken', {})
+
+        if (response.status === 200) {
+            return api(error.config)
+        }
+    }
+
+    refresh = false;
+    return error;
+});
