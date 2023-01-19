e419b7e54e2af41a88dcf5e908796980ad8ca145
Fábio Penha <fabiopenha95@outlook.com>
2023-01-19 20:57:49 -0300

    feat: adding context provider around app

--- src/index.tsx
+++ src/index.tsx
@@ -2,15 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { UserContextProvider } from "./context/authContext";
+import { ApiDataProvider } from "./context/getUserContext";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <UserContextProvider>
+    <ApiDataProvider>
       <App />
-    </UserContextProvider>
+    </ApiDataProvider>
   </React.StrictMode>
 );
