53a70606e4efbb2bde1f1e4f7a109534101a17aa
Fábio Penha <fabiopenha95@outlook.com>
2023-01-19 20:54:28 -0300

    feat: create get current user Context

---
+++ src/context/getUserContext.tsx
@@ -0,0 +1,34 @@
+import { ReactNode, useEffect, useState } from "react";
+import { createContext } from "react";
+import { api } from "../components/utils/api/api";
+
+interface IUser {
+    _id?: string;
+}
+
+interface IProps {
+    children: ReactNode;
+}
+
+//const {data} = await api.get('/v1/auth/authenticatedUser');
+    
+export const ApiDataContext = createContext({});
+
+export const ApiDataProvider = ({children} : IProps) => {
+    const [data, setData] = useState<IUser[]>([]);
+
+    useEffect(() => {
+        api.get('/v1/auth/authenticatedUser')
+            .then(res => {
+                setData(res.data.user._id);
+            })
+    }, [data]);
+
+    return (
+        <ApiDataContext.Provider value={data}>
+            {children}
+        </ApiDataContext.Provider>
+    );
+};
+
+
