d83e2680065f0e325630ecd9d29b96a01f7561bf
Fábio Penha <fabiopenha95@outlook.com>
2023-01-20 00:15:26 -0300

    fix: fix cards components

--- src/components/iconFaces/CardStatus.tsx
+++ src/components/iconFaces/CardStatus.tsx
@@ -3,15 +3,17 @@ type Props = {
   iconFace: string;
   color: string;
   text: string;
+  display:string;
 };
 
-const CardStatus = ({ iconFace, color, text }: Props) => {
+const CardStatus = ({ iconFace, color, text, display }: Props) => {
   return (
     <div
+      style={{backgroundColor: color }}
       className={`w-full h-[131px] bg-[${color}] mt-10 rounded-md p-3
-    flex items-center md:justify-center gap-3`}
+    flex items-center md:justify-center gap-3 ${display? '' : 'hidden'}`}
     >
-      <img src={iconFace} alt="sad" />
+      <img src={iconFace} alt="icon" />
       <p className="text-xs md:text-base font-bold text-white">{text}</p>
     </div>
   );
